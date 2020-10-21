10d2c12cb4e4d7171d7f9ae6437ff255816fe4b3
donezombie <donezombie@gmail.com>
2020-10-21 19:17:24 +0700

    remove redundant code

--- src/App.jsx
+++ src/App.jsx
@@ -21,34 +21,3 @@ const App = () => {
 }
 
 export default App;
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
-// import './scss/styles.scss';
-// import SecureRoute from 'routes/SecureRoute';
-// import PrivateRoute from 'components/PrivateRoute';
-// import { RouteBase } from 'constants/routeUrl';
-// import LoginPage from 'views/Login';
-
-// import * as Ons from 'react-onsenui';
-// import ons from 'onsenui';
-// import 'onsenui/css/onsenui.css';
-// import 'onsenui/css/onsen-css-components.css';
-
-// const App = () => {
-
-//   const handleClick = () => {
-//     ons.notification.alert('Hello world!');
-//   };
-
-//   // RENDER
-//   return (
-//     <Ons.Page>
-//       <Ons.Button onClick={handleClick}>Tap me!</Ons.Button>
-//       <Ons.BottomToolbar modifier="material"> Content </Ons.BottomToolbar>
-//     </Ons.Page>
-//   )
-  
-// }
-
-// export default App;
