b9b7c445a51c6963f84cb19235e45c3813334669
DonPQ <107664839+DonPQ@users.noreply.github.com>
2022-07-11 15:48:30 +0700

    improve project

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 
 import './scss/styles.scss';
-import SecureRoute from 'routes/SecureRoute';
 import PrivateRoute from 'components/PrivateRoute';
 import { RouteBase } from 'constants/routeUrl';
 import { checkAuth } from 'redux/modules/auth';
 import LoginPage from 'views/Login';
+import DefaultLayout from 'layout/DefaultLayout';
 
 const App = () => {
   const dispatch = useDispatch();
@@ -21,7 +21,7 @@ const App = () => {
     <Router>
       <Switch>
         <Route path={RouteBase.Login} exact component={LoginPage} />
-        <PrivateRoute path={RouteBase.Home} component={SecureRoute} />
+        <PrivateRoute path={RouteBase.Home} component={DefaultLayout} />
       </Switch>
     </Router>
   );
--- src/layout/DefaultLayout/index.jsx
+++ src/layout/DefaultLayout/index.jsx
@@ -1,14 +1,28 @@
+import React, { Fragment, Suspense } from 'react';
+import PrivateRoute from 'components/PrivateRoute';
 import Footer from 'components/Footer';
 import Header from 'components/Header';
-import React, { Fragment } from 'react';
+import routes from 'routes/routes';
+import { Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 
 const DefaultLayout = (props) => {
-  const { children } = props;
-
   return (
     <Fragment>
       <Header />
-      <main className="main-container">{children}</main>
+      <main className="main-container">
+        <Suspense fallback="Loading...">
+          <Switch>
+            {routes.map((route, idx) => {
+              if (route.isPrivate) {
+                return <PrivateRoute key={idx} path={route.path} exact={route.exact} component={route.component} />;
+              }
+
+              return <Route key={idx} path={route.path} exact={route.exact} component={route.component} />;
+            })}
+          </Switch>
+        </Suspense>
+      </main>
       <Footer />
     </Fragment>
   );
